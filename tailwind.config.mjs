4fdad602ba2ebedcb00fbb59116c005dc515070d
Bielis08 <bielmonteroroman08@gmail.com>
2024-09-30 18:32:50 +0200

    First commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,16 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			'page-100': '#edffe9',
+			'page-200': '#c7ffb5',
+			'page-300': '#a1ff81',
+			'page-400': '#9fffa9',
+			'page-500': '#9dffd1',
+			'1414': '#141414',
+			'black': '#000',
+			'white': '#FFF'
+		},
 	},
 	plugins: [],
 }
