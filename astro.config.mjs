416e7906e8ac8372539ee4a41b44ac0332052af4
Bielis08 <bielmonteroroman08@gmail.com>
2024-10-01 16:53:21 +0200

    Add to astro.config.mjs the vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel/serverless';
+
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: 'server',
+  adapter: vercel(),
 });
